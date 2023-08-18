2429c0a2e0193e20ec4a68a9a40caaa802fe3ba4
Justice <unankajustice@gmail.com>
2023-08-18 10:58:27 +0100

    love calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log("What is your name?");
+console.log("What is their name?");
+
+
+const loveCal = Math.random() * 100;
+loveCal = Math.floor(loveCal) + 1; 
+loveCal.getElementById("demo").innerHtml = "Your score is " + loveCal + "%"
+
+
+if(loveCal > 65){
+    console.log("Your score is " + loveCal + "%." + "You both make a wonderful couple, just keep working and exploring yourself") 
+
+}else{
+    console.log("Your score is " + loveCal + "%" + "There are more people out there who would love to be with you. Don't give up on love")
+}
