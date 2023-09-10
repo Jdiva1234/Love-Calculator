c99e5952b1901574e9407766af831d649f66e3d0
Justice <unankajustice@gmail.com>
2023-09-10 20:26:45 +0100

    Co-authored-by: David Akang <DBAKANG-GIT@users.noreply.github.com>

---
+++ CFG/CFG WEB/script.js
@@ -0,0 +1,7 @@
+const showBtn = document.getElementById("showBtn");
+const container = document.getElementById("container");
+const image = document.getElementById("image");
+showBtn.addEventListener("click", function () {
+image.src =
+    "https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg";
+});
--- script.js
+++ script.js
@@ -5,8 +5,7 @@ console.log("What is their name?");
 
 const loveCal = Math.random() * 100;
 loveCal = Math.floor(loveCal) + 1; 
-loveCal.getElementById("demo").innerHtml = "Your score is " + loveCal + "%"
-
+loveCal.getElementById("demo").innerHtml = "Your score is " + loveCal + "%";
 
 if(loveCal > 65){
     console.log("Your score is " + loveCal + "%." + "You both make a wonderful couple, just keep working and exploring yourself") 
@@ -14,3 +13,8 @@ if(loveCal > 65){
 }else{
     console.log("Your score is " + loveCal + "%" + "There are more people out there who would love to be with you. Don't give up on love")
 }
+
+
+
+const leapYear = 2023;
+2023
